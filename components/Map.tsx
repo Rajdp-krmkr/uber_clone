2189fdeaa7f2d173f042ffe01cc4c07e1a02957f
Rajdeep Karmakar <rajdeepkarmakar2027@gmail.com>
2025-02-14 00:10:49 +0530

    Update Map component to use PROVIDER_DEFAULT and adjust app configuration

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,22 +1,35 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { PROVIDER_DEFAULT, PROVIDER_GOOGLE } from 'react-native-maps'
 
 const Map = () => {
     // const region = {}
     return (
-        <MapView
-            provider={PROVIDER_GOOGLE}
-            className='w-full h-full rounded-2xl'
-            tintColor='black'
-            mapType='mutedStandard'
-            showsPointsOfInterest={false}
-            // initialRegion={region}
-            showsUserLocation={true}
-            userInterfaceStyle='light'
-        >
-            <Text>Map</Text>
-        </MapView>
+        <>
+            <Text>hi dsdsd </Text>
+            {/* <MapView
+                provider={PROVIDER_GOOGLE}
+                className='w-full h-full rounded-2xl'
+                tintColor='black'
+                mapType='mutedStandard'
+                showsPointsOfInterest={false}
+                // initialRegion={region}
+                showsUserLocation={true}
+                userInterfaceStyle='light'
+            >
+                <Text>Map</Text>
+            </MapView> */}
+            <MapView
+                provider={PROVIDER_DEFAULT}
+                style={{ flex: 1 }}
+                initialRegion={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }}
+            />
+        </>
     )
 }
 
